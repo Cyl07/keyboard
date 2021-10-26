b01a40e50b045cdcab5dba80bc43d98bd895d69a
Cyl <labetowiez@aol.fr>
2021-10-26 22:18:52 +0200

    added script to have a color indication

---
+++ public/assets/js/script.js
@@ -0,0 +1,13 @@
+let indices = document.getElementsByClassName("indice");
+
+for (let index in indices){
+    console.log(indices[index]);
+    let html = indices[index].innerHTML.split(" ")[0];
+    if (html < 1){
+        indices[index].parentElement.classList.add("good");
+    } else if(html < 2){
+        indices[index].parentElement.classList.add("average");
+    } else {
+        indices[index].parentElement.classList.add("bad");
+    }
+}
